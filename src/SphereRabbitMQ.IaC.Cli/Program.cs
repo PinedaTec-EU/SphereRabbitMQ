3c0f4da04c3427db813b65ddb4a9b560aa33d389
jmrpineda <jmr.pineda@pinedatec.eu>
2026-03-10 16:45:42 +0100

    Fix command invocation in Program.cs for improved argument handling

--- src/SphereRabbitMQ.IaC.Cli/Program.cs
+++ src/SphereRabbitMQ.IaC.Cli/Program.cs
@@ -1,3 +1,4 @@
+using System.CommandLine;
 using Microsoft.Extensions.DependencyInjection;
 using SphereRabbitMQ.IaC.Cli.Commands;
 using SphereRabbitMQ.IaC.Cli.DependencyInjection;
@@ -21,6 +22,6 @@ public static class Program
             .BuildServiceProvider();
 
         var rootCommand = TopologyRootCommandFactory.Create(services);
-        return rootCommand.Parse(args).InvokeAsync();
+        return rootCommand.InvokeAsync(args);
     }
 }
