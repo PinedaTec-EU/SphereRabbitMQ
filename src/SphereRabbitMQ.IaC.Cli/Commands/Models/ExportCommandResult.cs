4a1e1993799b9df055341ce1e4181a75e1c2414e
jmrpineda <jmr.pineda@pinedatec.eu>
2026-03-11 19:27:16 +0000

    feat: add [ExcludeFromCodeCoverage] attribute to ExportCommandResult

--- src/SphereRabbitMQ.IaC.Cli/Commands/Models/ExportCommandResult.cs
+++ src/SphereRabbitMQ.IaC.Cli/Commands/Models/ExportCommandResult.cs
@@ -1,7 +1,10 @@
+using System.Diagnostics.CodeAnalysis;
+
 using SphereRabbitMQ.IaC.Application.Models;
 
 namespace SphereRabbitMQ.IaC.Cli.Commands.Models;
 
+[ExcludeFromCodeCoverage]
 internal sealed record ExportCommandResult(
     BrokerResolutionResult Broker,
     string Content,
